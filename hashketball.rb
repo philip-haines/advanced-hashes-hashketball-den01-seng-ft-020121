62528976ef4c37aa0886782ce715928db1aadbe9
eager-monitor-013e <phil.haines92@gmail.com>
2021-01-09 01:04:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -11,7 +11,7 @@ def game_hash
           number: 0,
           shoe: 16,
           points: 22,
-          rebounds: 12,
+          rebounds: 11,
           assists: 12,
           steals: 3,
           blocks: 1,
