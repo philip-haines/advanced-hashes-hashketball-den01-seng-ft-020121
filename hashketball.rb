f9101bb5b66e72db4c9e936470dcd480f567604f
eager-monitor-013e <phil.haines92@gmail.com>
2021-01-09 00:50:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -198,6 +198,7 @@ def big_shoe_rebounds
   game_hash.each do |key, value|
     value[:players].each do |stats|
       if stats[:shoe] > biggest_shoe_size
+        stats[:shoe] = biggest_shoe_size
         rebounds = stats[:rebounds]
       end
     end
